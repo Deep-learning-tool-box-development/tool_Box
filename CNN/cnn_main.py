7edab5e0690f8e10a040489e214e418bcfcea4d0
Xleaner <zhengqie.manji@gmail.com>
2021-04-02 23:31:25 +0200

    Update cnn_main.py
    change run_cnn_sa function, including position of var_size and the value of alpha

--- CNN/cnn_main.py
+++ CNN/cnn_main.py
@@ -19,11 +19,10 @@ def run_cnn_pso(path_to_data, var_size):
     pso.run()
 
 
-def run_cnn_sa(path_to_data):
+def run_cnn_sa(path_to_data, var_size):
     x_train, x_test, y_train, y_test = import_data(path_to_data, model='CNN')
     cnn = CNN(x_train, y_train, x_test, y_test)
-    var_size = [[0.5, 0.8], [0.001, 0.01], [1, 3.5]]
-    sa = SA(cnn.cnn_get_score, 90, 0.1, 0.01, var_size)
+    sa = SA(cnn.cnn_get_score, 90, 0.1, 0.92, var_size)
     sa.run()
 
 
@@ -46,4 +45,4 @@ if __name__ == '__main__':
         run_cnn_pso(path, var_size)
 
     elif option == 'SA':
-        run_cnn_sa(path)
+        run_cnn_sa(path, var_size)
