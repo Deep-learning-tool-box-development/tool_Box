155db572d1b8b2c1f731b3de94d7d1c29ef42a26
Xleaner <zhengqie.manji@gmail.com>
2021-04-02 23:28:19 +0200

    Update sa.py
    Change iteration way and add print function

--- sa.py
+++ sa.py
@@ -13,7 +13,7 @@ class SA():
         :param objective: cost function as an objective
         :param initial_temp: double, manually set initial_temp, e.g. 90
         :param final_temp: double, stop_temp, e.g. 0.1
-        :param alpha: double, temperature changing step
+        :param alpha: double, temperature changing step, [0.985, 0.999]
         :param var_size: list, upper and lower bounds of each parameter
         :param num_itr: int, iteration times
         """
@@ -21,50 +21,70 @@ class SA():
         self.objective = objective  # Objective network to be optimize
         self.initial_temp = initial_temp  # 90
         self.final_temp = final_temp  # .1
-        self.alpha = alpha  # 0.01 衰减因子
+        self.alpha = alpha  # 0.92 衰减因子
         self.var_size = var_size  # [[],[],[]]
         self.dim = np.zeros(len(var_size))
-       # self.num_itr = num_itr
+        self.Global_Best = []
+        self.Best_Cost = []
 
     def run(self):
         """
         :return: current state, current cost, state lists, cost lists
         """
         """ Optimize object network with the simulated annealing algorithm."""
-        #for i in range(self.num_itr):
-        initial_state = self.random_start()  # start from a random state, multiple dimension
+
+        initial_state = self._random_start()  # start from a random state, multiple dimension
         current_temp = self.initial_temp
         current_state = initial_state
         cost = self.objective(current_state)
         solution = current_state
         states, costs = [current_state], [cost]
+        num_itr = 1
         while current_temp > self.final_temp:
-            # print("current_state", current_state)
-            neighbour_of_current = self.random_neighbour(current_state)
+            print("iteration", num_itr, "...")
+            neighbour_of_current = self._random_neighbour(solution)
             # print("now_neighbor", neighbour_of_current)
-            # Check if neighbor is best so far
-            print("out_current_state", self.objective(current_state), "out_neighbour", self.objective(neighbour_of_current))
-            cost_diff = self.objective(current_state) - self.objective(neighbour_of_current)
-            # print("cost_diff", cost_diff)
-
+            # Check if neighbor is best so far           
+            cost_current = self.objective(solution)
+            cost_neighbour = self.objective(neighbour_of_current)
+            cost_diff = cost_current - cost_neighbour
+            print("cost_current_state", cost_current, "cost_neighbour", cost_neighbour)
+            print("cost_diff", cost_diff)
             # if the new solution is better, accept it
             if cost_diff > 0:
                 solution = neighbour_of_current
+                cost = cost_neighbour
                 # if the new solution is not better, accept it with a probability of e^(-cost/temp)
             else:
-                if np.random.uniform(0, 1) < math.exp(-cost_diff / current_temp):
+                if np.random.uniform(0, 1) < math.exp(cost_diff / current_temp):
                     solution = neighbour_of_current
+                    cost = cost_neighbour
+                else: 
+                  solution = current_state
+                  cost = cost_current
                 # decrement the temperature
-            states.append(current_state)
-            current_temp -= self.alpha
-            costs.append(self.objective(solution))
-        print(#'iteration', i + 1, ': Cost=', self.objective(solution),
-                'Best parameters: ',
-              '\ndropout=', solution[0],
-              'learning rate=', solution[1],
-              'batch size=', int(solution[2]))
+            print("cost", cost)
+            states.append(solution)
+            costs.append(cost)
+            current_temp = current_temp*self.alpha
+            num_itr += 1
+            print(
+                "Best parameters: ",
+              "\ndropout=", solution[0],
+              "learning rate=", solution[1],
+              "batch size=", int(solution[2]),
+              "number of convolution=", solution[3])
 
-    def random_start(self):
+        plt.plot(costs)
+    #
+    # def plot_curve(self):
+    #     """
+    #     Plot optimizer curve with iteration
+    #     :return: None
+    #     """
+    #     plt.plot()
+
+    def _random_start(self):
         """ Random point in the interval """
         print("___START____")
         rd_state = self.dim
@@ -75,7 +95,7 @@ class SA():
         print("init_random_state", rd_state)
         return rd_state
 
-    def random_neighbour(self, state):  # fraction_origin=1
+    def _random_neighbour(self, state):  # fraction_origin=1
         """Find neighbour of current state"""
         neighbour = self.dim
         for j in range(len(self.dim)):
