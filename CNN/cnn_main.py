dd9e8d834e1f45aa6b0cafd7af71d3b026eea2ad
Xleaner <zhengqie.manji@gmail.com>
2021-04-03 08:51:03 +0200

    Update cnn_main.py
    add annotation for run_cnn_sa

--- CNN/cnn_main.py
+++ CNN/cnn_main.py
@@ -20,6 +20,12 @@ def run_cnn_pso(path_to_data, var_size):
 
 
 def run_cnn_sa(path_to_data, varsize):
+    """
+    Main function for the CNN and SA.
+
+    :param path_to_data: string, Folder of the data files
+    :return: None
+    """
     x_train, x_test, y_train, y_test = import_data(path_to_data, model='CNN')
     cnn = CNN(x_train, y_train, x_test, y_test)
     sa = SA(cnn.cnn_get_score, 90, 0.1, 0.92, var_size, net="CNN")
