53ace5a3c3fa2185380f6ada24ad8fd5a02282ad
Xleaner <zhengqie.manji@gmail.com>
2021-04-03 09:09:23 +0200

    Update knn.py
    update KNN

--- knn.py
+++ knn.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 from dataset import import_data
 from sklearn.model_selection import train_test_split
+from utils import data_FFT
 
 
 def run_knn(path_to_data):
@@ -19,8 +20,10 @@ def run_knn(path_to_data):
     :return: None
     """
     x_train, x_test, y_train, y_test = import_data(path_to_data, model='KNN')
+    x_train = data_FFT(x_train)
+    x_test = data_FFT(x_test)
     knn = KNN(x_train, y_train, x_test, y_test)
-    k_range = range(1, 15)
+    k_range = range(1, 25)
     weight_choices = ["uniform", "distance"]
     params = [k_range, weight_choices]  # editable params
     knn.predict(params)
@@ -34,7 +37,6 @@ class KNN():
         self.y_train = y_train.ravel()
         self.x_test = x_test
         self.y_test = y_test.ravel()
-        # self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(x_train, y_train.ravel(), test_size=0.1)
 
     # def save_model(self):
     #     from sklearn.externals import joblib
@@ -70,39 +72,31 @@ class KNN():
                 knn = KNeighborsClassifier(n_neighbors=k, weights=weights, metric='minkowski', p=2)
                 # params[2] and params[3] Default is "minkowski" with p=2, equivalent to the standard Euclidean metric
                 # e.g. [5, "uniform",'minkowski', 2]
-                scores = cross_val_score(knn, X_train, self.y_train, scoring='accuracy')
-                Error = 1 - scores.mean()
-                k_error.append(Error)
+                # scores = cross_val_score(knn, X_train, self.y_train, scoring='accuracy')
+                # Error = 1 - scores.mean()
+                # k_error.append(Error)
 
-                # knn.fit(X_train, self.y_train)
-                # Error = 1 - knn.score(X_test, self.y_test)
-                # result = knn.predict(X_test)
-                # self.result = result
+                knn.fit(X_train, self.y_train)  # train on train_file
+                Error = 1 - knn.score(X_test, self.y_test)  # error on test_file
+                result = knn.predict(X_test)
+                self.result = result
                 print("k=", k)
                 print("weights=", weights)
                 # print("knn_result", result)
                 print("Error", Error)
-                # k_error.append(Error)
+                k_error.append(Error)
+            # plot Error_list on variable k value
             plt.plot(k_range, k_error)
             plt.title("Error under different choice of K")
             plt.xlabel("Value of K for KNN")
             plt.ylabel("Error")
             plt.show()
 
-        # Predict the Test set results
-        # fig = plt.figure()
-        # ax = plt.axes(projection='3d')
-        # ax.contour3D(k_range, weight_choices, k_error, 50, cmap='binary')
-        # ax.set_xlabel('Value of K for KNN')
-        # ax.set_ylabel('Weight for KNN')
-        # ax.set_zlabel('Error')
-        # ax.set_title('3D contour')
-
 
 if __name__ == "__main__":
     """
-    Main function to call the selected model 
+    Main function to call the knn
     """
     # path = "PATH TO DATASET"
-    path = "C:\\Users\\zhudifan\\Desktop\\DL\\dataset\\"
+    path = './dataset/'
     run_knn(path)
