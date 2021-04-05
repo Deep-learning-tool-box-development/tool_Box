2612b7dc868f553ec80d11d10167e2bd186bbbbc
Xleaner <zhengqie.manji@gmail.com>
2021-04-05 11:44:51 +0200

    Update sa.py
    debug list self.dim = np.zeros(len(var_size))

--- CNN/sa.py
+++ CNN/sa.py
@@ -8,12 +8,12 @@ import math
 
 class SA():
 
-    def __init__(self, objective, initial_temp, final_temp, alpha, var_size, net="CNN"):
+    def __init__(self, objective, initial_temp, final_temp, alpha, var_size, net="DBN"):
         """
         :param objective: cost function as an objective
         :param initial_temp: double, manually set initial_temp, e.g. 90
         :param final_temp: double, stop_temp, e.g. 0.1
-        :param alpha: double, temperature changing step, [0.985, 0.999]
+        :param alpha: double, temperature changing step, [0.900, 0.999]
         :param var_size: list, upper and lower bounds of each parameter
         :param num_itr: int, iteration times
         """
@@ -26,49 +26,56 @@ class SA():
         self.dim = np.zeros(len(var_size))
         self.net = net
 
-
     def run(self):
         """
-        :return: current state, current cost, state lists, cost lists
+        :return: cost of all iteration and objective functions' optimized parameters
         """
         """ Optimize object network with the simulated annealing algorithm."""
 
-        initial_state = self._random_start()  # start from a random state, multiple dimension
+        state = self._random_start()  # start from a random state, multiple dimension
         current_temp = self.initial_temp
-        current_state = initial_state
-        cost = self.objective(current_state)
-        solution = current_state
-        states, costs = [current_state], [cost]
+        cost = self.objective(state)
+        states, costs = [state], [cost]
         num_itr = 1
         while current_temp > self.final_temp:
-            print("iteration", num_itr, "...")
-            neighbour_of_current = self._random_neighbour(solution)
-            # print("now_neighbor", neighbour_of_current)
-            # Check if neighbor is best so far           
-            cost_current = self.objective(solution)
-            cost_neighbour = self.objective(neighbour_of_current)
-            cost_diff = cost_current - cost_neighbour
-            print("cost_current_state", cost_current, "cost_neighbour", cost_neighbour)
+            print("====iteration====", num_itr, "...")          
+            old_state = state
+            print("state0", state)
+            new_state = self._random_neighbour(old_state)
+            print("state1", state)
+            print("new_state", new_state)
+            # Check if neighbor is best so far                      
+            new_cost = self.objective(new_state)
+            cost_diff = new_cost - cost            
+            print("cost", cost, "new_cost", new_cost)
             print("cost_diff", cost_diff)
             # if the new solution is better, accept it
-            if cost_diff > 0:
-                solution = neighbour_of_current
-                cost = cost_neighbour
+            if cost_diff < 0:
+                state = new_state
+                cost = new_cost
+                print("==>accept new")
                 # if the new solution is not better, accept it with a probability of e^(-cost/temp)
-            else:
-                if np.random.uniform(0, 1) < math.exp(cost_diff / current_temp):
-                    solution = neighbour_of_current
-                    cost = cost_neighbour
-                else: 
-                  solution = solution
-                  cost = cost_current
+            elif cost_diff >= 0:
+                if np.random.uniform(0, 1) > math.exp(-cost_diff / current_temp):
+                    state = new_state
+                    cost = new_cost
+                    states.append(state)
+                    costs.append(cost)
+                    print("==>accept new")
+                else:
+                  print("==>reject new")
+                # else: 
+                #   solution = solution
+                #   cost = cost_current
                 # decrement the temperature
+            print("solution", state)
             print("cost", cost)
-            states.append(solution)
-            costs.append(cost)
+            print("T", current_temp)
+            
             current_temp = current_temp*self.alpha
             num_itr += 1
-            print_params(solution, net=self.net)
+            print_params(state, net=self.net)
+            
 
         plt.plot(costs)
     #
@@ -82,21 +89,23 @@ class SA():
     def _random_start(self):
         """ Random point in the interval """
         print("___START____")
-        rd_state = self.dim
-        for i in range(len(self.dim)):
+        rd_state = np.zeros(len(var_size))
+        for i in range(len(np.zeros(len(var_size)))):
             #print("rd_point", rd_state)
             rd_point = np.random.uniform(self.var_size[i][0], self.var_size[i][1])
             rd_state[i] = rd_point
         print("init_random_state", rd_state)
         return rd_state
 
-    def _random_neighbour(self, state):  # fraction_origin=1
+    def _random_neighbour(self, state_old):  
         """Find neighbour of current state"""
-        neighbour = self.dim
-        for j in range(len(self.dim)):
+        print("___NEIGHBOUR____")
+
+        neighbour = np.zeros(len(var_size))
+        for j in range(len(np.zeros(len(var_size)))):
             amplitude = (self.var_size[j][1] - self.var_size[j][0]) * 1 / 10
             delta = (-amplitude / 2.) + amplitude * np.random.random_sample()
-            neighbour_point = max(min(state[j] + delta, self.var_size[j][1]), self.var_size[j][0])
-            neighbour[j] = neighbour_point
-        # print("neighbor", neighbour)
+            middle_point = state_old[j]
+            neighbour_point = max(min(middle_point + delta, self.var_size[j][1]), self.var_size[j][0])           
+            neighbour[j] = neighbour_point       
         return neighbour
