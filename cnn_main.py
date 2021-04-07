513058aed21a5ab9bafa573553fd60dcddd9a472
Xleaner <zhengqie.manji@gmail.com>
2021-04-07 23:17:55 +0200

    Add files via upload

---
+++ cnn_main.py
@@ -0,0 +1,62 @@
+
+from dataset import import_data
+from pso import PSO
+from cnn import CNN
+from sa import SA
+
+
+def run_cnn_pso(path_to_data, var_size, discrete_candidate):
+    """
+    Main function for the CNN and PSO.
+
+    :param path_to_data: string, Folder of the data files
+    :return: None
+    """
+    # create the CNN model
+    x_train, x_test, y_train, y_test = import_data(path_to_data, model='CNN')
+    cnn = CNN(x_train, y_train, x_test, y_test,
+              discrete_candidate=discrete_candidate,
+              optimization=True,
+              epoch=6)
+    pso = PSO(cnn.cnn_get_score, 5, 5, var_size, candidate=discrete_candidate, net="CNN")
+    pso.run()
+
+
+def run_cnn_sa(path_to_data, var_size, discrete_candidate):
+    """
+    Main function for the CNN and SA.
+    :param var_size: list, upper and under boundaries of all variables
+    :param path_to_data: string, Folder of the data files
+    :return: None
+    """
+    x_train, x_test, y_train, y_test = import_data(path_to_data, model='CNN')
+    cnn = CNN(x_train, y_train, x_test, y_test,
+              discrete_candidate=discrete_candidate,
+              optimization=True,
+              epoch=6)
+    sa = SA(cnn.cnn_get_score, 200, 10, 0.9, var_size, candidate=discrete_candidate, net="CNN")
+    sa.run()
+
+
+if __name__ == '__main__':
+    """
+    Main function to call the selected model and optimizer
+    """
+    # Main function
+    option = 'SA'
+    path = './Dataset/'
+    # below should get from config
+    do = [0.3, 0.8]
+    lr = [0.0001, 0.02]
+    # 6 candidate of batch size
+    conv_candidate = [4, 6, 8]
+    bs_candidate = [1, 16, 32, 64, 128, 256]
+    discrete_candi = [bs_candidate, conv_candidate]
+    # 3 candidate of number of the convolution layers
+    var_size = [do, lr, [0, 1], [0, 1]]
+    if option == 'PSO':
+        run_cnn_pso(path, var_size, discrete_candi)
+
+    elif option == 'SA':
+        run_cnn_sa(path, var_size, discrete_candi)
+
---
+++ sa.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+""" SA optimizer """
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+from utils import print_params
+
+
+class SA:
+
+    def __init__(self, objective, initial_temp, final_temp, alpha, var_size, candidate=None, net="DBN"):
+        """
+        :param objective: cost function as an objective
+        :param initial_temp: double, manually set initial_temp, e.g. 90
+        :param final_temp: double, stop_temp, e.g. 0.1
+        :param alpha: double, temperature changing step, [0.900, 0.999]
+        :param var_size: list, upper and lower bounds of each parameter
+        """
+        self.interval = (0, 1)  # set a range (0,1)
+        self.objective = objective  # Objective network to be optimize
+        self.initial_temp = initial_temp  # 90
+        self.final_temp = final_temp  # .1
+        self.alpha = alpha  # 0.92 衰减因子
+        self.var_size = var_size  # [[],[],[]]
+        self.dim = np.zeros(len(var_size))
+        self.net = net
+        self.candidate = candidate
+
+    def run(self):
+        """
+        :return: cost of all iteration and objective functions' optimized parameters
+        """
+        """ Optimize object network with the simulated annealing algorithm."""
+
+        state = self._random_start()  # start from a random state, multiple dimension
+        self.current_temp = self.initial_temp
+        self.temp = [self.current_temp]
+        cost = self.objective(state)
+        self.states, self.costs = [state], [cost]
+        num_itr = 1
+        while self.current_temp > self.final_temp:
+            print("====iteration====", num_itr, "...")
+            old_state = state
+            print("state0", state)
+            new_state = self._random_neighbour(old_state)
+            print("state1", state)
+            print("new_state", new_state)
+            # Check if neighbor is best so far
+            new_cost = self.objective(new_state)
+            cost_diff = new_cost - cost
+            print("cost", cost, "new_cost", new_cost)
+            print("cost_diff", cost_diff)
+            # if the new solution is better, accept it
+            if cost_diff < 0:
+                state = new_state
+                cost = new_cost
+                print("==>accept new")
+                # if the new solution is not better, accept it with a probability of e^(-cost/temp)
+            elif cost_diff >= 0:
+                if np.random.uniform(0, 1) < math.exp(-(cost_diff*1.38064852*10**23) / self.current_temp):
+                    state = new_state
+                    cost = new_cost
+                    print("==>accept new")
+                else:
+                    print("==>reject new")
+            self.states.append(state)
+            self.costs.append(cost)
+            self.temp.append(self.current_temp)
+            print("solution", state)
+            print("cost", cost)
+            print("T", self.current_temp)
+            # reduce the temperature
+            self.current_temp = self.current_temp*self.alpha
+            num_itr += 1
+            print_params(state, self.candidate, net=self.net)
+            print(len(self.temp), len(self.costs))
+        self.plot_curve()
+    def plot_curve(self):
+        """
+        Plot optimizer curve with iteration
+        :return: None
+        """
+        plt.plot(self.costs)
+        plt.ylabel("Objective costs")
+        plt.xlabel("Iteration")
+        plt.show()
+
+    def _random_start(self):
+        """ Random point in the interval """
+        print("___START____")
+        rd_state = np.zeros(len(self.var_size))
+        for i in range(len(np.zeros(len(self.var_size)))):
+            #print("rd_point", rd_state)
+            rd_point = np.random.uniform(self.var_size[i][0], self.var_size[i][1])
+            rd_state[i] = rd_point
+        print("init_random_state", rd_state)
+        return rd_state
+
+    def _random_neighbour(self, state_old):
+        """Find neighbour of current state"""
+        print("___NEIGHBOUR____")
+
+        neighbour = np.zeros(len(self.var_size))
+        for j in range(len(np.zeros(len(self.var_size)))):
+            amplitude = (self.var_size[j][1] - self.var_size[j][0]) * 1 / 10
+            delta = (-amplitude / 2.) + amplitude * np.random.random_sample()
+            middle_point = state_old[j]
+            neighbour_point = max(min(middle_point + delta, self.var_size[j][1]), self.var_size[j][0])
+            neighbour[j] = neighbour_point
+        return neighbour
