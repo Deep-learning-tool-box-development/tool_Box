8d1b46cb15f4bccfc3705ab369e18c29badd24f1
lengyu1996 <66223936+lengyu1996@users.noreply.github.com>
2021-04-13 00:19:14 +0200

    change SVM

--- svm_main.py
+++ svm_main.py
@@ -12,13 +12,14 @@ def run_svm_pso(path_to_data, var_size):
     :param var_size: list, upper and under boundaries of all variables
     :return: None
     """
-    x_train, x_test, y_train, y_test = import_data(path_to_data, model = 'SVM')
-    svm = SVM_Model(x_train = x_train, y_train = y_train,
-                    x_test = x_test, y_test = y_test,
+    x_train, x_test, y_train, y_test = import_data(path_to_data, model='SVM')
+    svm = SVM_Model(x_train=x_train, y_train=y_train,
+                    x_test=x_test, y_test=y_test,
                     optimization=True)
     pso = PSO(svm.get_score, 2, 5, var_size, net="SVM")
     pso.run()
-    
+
+
 def run_svm_sa(path_to_data, var_size):
     """
     Main function for the SVM and SA.
@@ -26,24 +27,24 @@ def run_svm_sa(path_to_data, var_size):
     :param var_size: list, upper and under boundaries of all variables
     :return: None
     """
-    x_train, x_test, y_train, y_test = import_data(path_to_data, model = 'SVM')
-    svm = SVM_Model(x_train = x_train, y_train = y_train,
-                    x_test = x_test, y_test = y_test,
-                    optimization = True)
-    sa = SA(svm.get_score, 200, 10, 0.9, var_size, net = "SVM")
+    x_train, x_test, y_train, y_test = import_data(path_to_data, model='SVM')
+    svm = SVM_Model(x_train=x_train, y_train=y_train,
+                    x_test=x_test, y_test=y_test,
+                    optimization=True)
+    sa = SA(svm.get_score, 200, 10, 0.9, var_size, net="SVM")
     sa.run()
-    
+
 
 if __name__ == '__main__':
     """
     Main function to call the selected model and optimizer
     """
     # Main function
-    option = "SA" # switch between "SA", "PSO"
+    option = "SA"  # switch between "SA", "PSO"
     path = './dataset/'
     # below should get from config
 
-    var_size=[[15, 100], [0.001, 0.01]] # var_size = [C,gamma]
+    var_size = [[15, 100], [0.001, 0.01]]  # var_size = [C,gamma]
 
     if option == 'PSO':
         run_svm_pso(path, var_size)
