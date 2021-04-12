02dbb533e8e6a4c1cbfd2bf833b37ad87f318743
lengyu1996 <66223936+lengyu1996@users.noreply.github.com>
2021-04-13 00:06:40 +0200

    change SVM

--- svm_main.py
+++ svm_main.py
@@ -15,7 +15,7 @@ def run_svm_pso(path_to_data, var_size):
     x_train, x_test, y_train, y_test = import_data(path_to_data, model = 'SVM')
     svm = SVM_Model(x_train = x_train, y_train = y_train,
                     x_test = x_test, y_test = y_test,
-                    C = C, gamma = gamma, optimization=True)
+                    optimization=True)
     pso = PSO(svm.get_score, 2, 5, var_size, net="SVM")
     pso.run()
     
