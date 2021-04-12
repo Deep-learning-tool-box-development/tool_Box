e14d7d24ee808ede0025f52301dc274dd8c6bd7b
ZhengQieCHSH <zhengqie.manji@gmail.com>
2021-04-12 16:35:55 +0200

    add knn best k and error from iteration

--- knn.py
+++ knn.py
@@ -37,16 +37,8 @@ class KNN():
         self.y_train = y_train.ravel()
         self.x_test = x_test
         self.y_test = y_test.ravel()
-        self.result = 0  # initialisation
-
-    # def save_model(self):
-    #     from sklearn.externals import joblib
-    #     joblib.dump(KNN, 'knn_model.pkl')
-    #     # load model
-    #     SVM_model = joblib.load('knn_model.pkl')
-    #     # make report
-    #     from sklearn import metrics
-    #     metrics.classification_report(self.y_test, self.result)
+        self.result_error = 0  # initialisation
+        self.result_k = 0  # initialisation
 
     def predict(self, params):
         """
@@ -73,20 +65,18 @@ class KNN():
                 knn = KNeighborsClassifier(n_neighbors=k, weights=weights, metric='minkowski', p=2)
                 # params[2] and params[3] Default is "minkowski" with p=2, equivalent to the standard Euclidean metric
                 # e.g. [5, "uniform",'minkowski', 2]
-                # scores = cross_val_score(knn, X_train, self.y_train, scoring='accuracy')
-                # Error = 1 - scores.mean()
-                # k_error.append(Error)
 
                 knn.fit(X_train, self.y_train)  # train on train_file
                 Error = 1 - knn.score(X_test, self.y_test)  # error on test_file
-                result = knn.predict(X_test)
-                self.result = result
+
                 print("k=", k)
                 print("weights=", weights)
-                # print("knn_result", result)
                 print("Error", Error)
                 k_error.append(Error)
             # plot Error_list on variable k value
+            self.result_error = min(k_error)
+            self.result_k = k_error.index(self.result_error)
+            print("best result", "k=", self.result_k, "lowest error=", self.result_error)
             plt.plot(k_range, k_error)
             plt.title("Error under different choice of K")
             plt.xlabel("Value of K for KNN")
