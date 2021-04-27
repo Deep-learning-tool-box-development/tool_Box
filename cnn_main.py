f09706b21f7914c1d37bff1fb950b181d8f0cac7
Yan Wang <64747300+wy-moonind@users.noreply.github.com>
2021-04-27 22:15:44 +0200

    value change

--- cnn_main.py
+++ cnn_main.py
@@ -20,7 +20,7 @@ def run_cnn_pso(path_to_data, var_size, discrete_candidate):
               discrete_candidate=discrete_candidate,
               optimization=True,
               epoch=6)
-    pso = PSO(cnn.cnn_get_score, 5, 5, var_size, candidate=discrete_candidate, net="CNN")
+    pso = PSO(cnn.cnn_get_score, 4, 15, var_size, candidate=discrete_candidate, net="CNN")
     pso.run()
 
 
